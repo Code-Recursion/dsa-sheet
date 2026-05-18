60de58359d0d79dd6eb32a2aa605c678fdd64999
Ajay Singh <helloajaysingh1@gmail.com>
2026-05-18 17:26:43 +0530

    rem env access from top lvl

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-console.log('MONGODB_URI',MONGODB_URI)
-
-if (!MONGODB_URI) {
-  throw new Error("Missing MONGODB_URI");
-}
-
 export async function connectDB() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  console.log("MONGODB_URI", MONGODB_URI);
+
+  if (!MONGODB_URI) {
+    throw new Error("Missing MONGODB_URI");
+  }
+
   if (mongoose.connection.readyState >= 1) {
     return;
   }
